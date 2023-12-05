b989cd441f67ef6d5f8b418e31919d150f743ed9
Mads Ludvig Timm Fagerlund <mads@fagerlund.io>
2023-12-05 17:11:56 +0100

    Move get_function to triage_level.c

--- src/patient_queue.c
+++ src/patient_queue.c
@@ -1,9 +1,4 @@
 #include "patient_queue.h"
-#include "symptoms.h"
-#include "vitals.h"
-#include <math.h>
-#include <stdbool.h>
-#include <stdio.h>
 #include <stdlib.h>
 
 struct patient_queue {
@@ -13,26 +8,6 @@ struct patient_queue {
   struct patient_node *green_head;
   struct patient_node *blue_head;
 };
-
-enum Triage_Level get_triage(struct Patient patient) {
-
-  enum Triage_Level triage_level = Blue;
-
-  if (patient.vitals != NULL) {
-    triage_level = get_vital_triage(*patient.vitals);
-  }
-
-  if (patient.symptoms_head) {
-    enum Triage_Level symptoms_triage =
-        get_symptoms_triage(*patient.symptoms_head);
-
-    if (triage_level > symptoms_triage) {
-      return symptoms_triage;
-    }
-  }
-
-  return triage_level;
-}
 
 struct patient_node *insert_tail(struct patient_node *cl, void *el) {
   struct patient_node *new_node =
--- src/triage_level.c
+++ src/triage_level.c
@@ -1,1 +1,24 @@
 #include "triage_level.h"
+#include "symptoms.h"
+#include "vitals.h"
+#include <stdlib.h>
+
+enum Triage_Level get_triage(struct Patient patient) {
+
+  enum Triage_Level triage_level = Blue;
+
+  if (patient.vitals != NULL) {
+    triage_level = get_vital_triage(*patient.vitals);
+  }
+
+  if (patient.symptoms_head) {
+    enum Triage_Level symptoms_triage =
+        get_symptoms_triage(*patient.symptoms_head);
+
+    if (triage_level > symptoms_triage) {
+      return symptoms_triage;
+    }
+  }
+
+  return triage_level;
+}

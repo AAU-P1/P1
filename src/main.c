f2b746f2084edc5aee24faa4ba85f40c4cc8e0a1
Anton <amve23@student.aau.dk>
2023-12-06 12:51:58 +0100

    systolic blood preasure

--- src/main.c
+++ src/main.c
@@ -193,7 +193,7 @@ void input_vitals(struct Patient *patient) {
     }
   }
 
-  patient->vitals->systolic_blood_pressure = input_int("Enter the systolic blood preasure");
+  patient->vitals->systolic_blood_pressure = input_int("Enter the systolic blood preasures");
 
 
 }

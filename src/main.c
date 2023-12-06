2ee9b690ea76a8be2d2cbd10f3e001e32a1039a2
Mads Ludvig Timm Fagerlund <mads@fagerlund.io>
2023-12-06 10:28:38 +0100

    Rename project1 to main, Move gui code to main

---
+++ src/main.c
@@ -0,0 +1,44 @@
+#include "patient_queue.h"
+#include "symptoms.h"
+#include "vitals.h"
+#include <ncurses.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+
+  // Make Anton and triage him
+  struct Vitals anton_vitals;
+  anton_vitals.airway = Free;
+  anton_vitals.oxygen_saturation = 100;
+  anton_vitals.with_oxygen = false;
+  anton_vitals.with_kol = false;
+  anton_vitals.respiration_frequency = 10;
+  anton_vitals.pulse = 60;
+  anton_vitals.systolic_blood_pressure = 100;
+  anton_vitals.glasgow_coma_scale = 10;
+  anton_vitals.temperature_celcius = 37;
+
+  struct Patient patient;
+  strcpy(patient.name, "Anton");
+  patient.age = 21;
+  patient.gender = Female;
+  patient.vitals = &anton_vitals;
+  patient.symptoms_head = NULL;
+
+  printf("triage_level anton: %d\n", get_triage(patient));
+
+  initscr();
+  cbreak();
+  noecho();
+
+  WINDOW *win = newwin(0, 0, 0, 0);
+  refresh();
+
+  mvwprintw(win, 15, 50, "Hello Friend");
+
+  wrefresh(win);
+  getch();
+
+  endwin();
+}

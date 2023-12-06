aac7b14129d108c9be951dfa0da05d91ab000029
Mads Ludvig Timm Fagerlund <mads@fagerlund.io>
2023-12-06 12:33:07 +0100

    Begin adding of input functions

--- src/main.c
+++ src/main.c
@@ -5,6 +5,10 @@
 #include <stdio.h>
 #include <string.h>
 
+void clearInputBuffer();
+void input_patient();
+void input_vitals(struct Patient *patient);
+
 int main(void) {
 
   // Make Anton and triage him
@@ -27,4 +31,171 @@ int main(void) {
   patient.symptoms_head = NULL;
 
   printf("triage_level anton: %d\n", get_triage(patient));
+
+  char choice;
+
+  printf("(T)riage new patient      (I)nput patient from file       (R)emove "
+         "Patient         (D)isplay Queue\n");
+  choice = getchar();
+  clearInputBuffer();
+
+  switch (choice) {
+  case 'T':
+  case 't':
+    triage_new_patient();
+    break;
+  case 'I':
+  case 'i':
+    break;
+  case 'R':
+  case 'r':
+    break;
+  case 'D':
+  case 'd':
+    break;
+  default:
+    printf("Invalid input try again");
+  }
+}
+
+int input_int(char *message) {
+  bool found = false;
+  while (found == false) {
+    printf("%s\n", message);
+    int number;
+    int res = scanf("%d", &number);
+    clearInputBuffer();
+    if (res != 1) {
+      printf("Invalid input. Try again!\n");
+    } else {
+      found = true;
+      return number;
+    }
+  }
+}
+
+void input_patient() {
+
+  struct Patient patient;
+
+  bool name_found = false;
+  while (!name_found) {
+    printf("Input patient name\n");
+    int res = scanf("%s", &patient.name);
+    clearInputBuffer();
+    if (res != 1) {
+      printf("Invalid input. Try again!\n");
+    } else {
+      name_found = true;
+    }
+  }
+
+  patient.age = input_int("Input patient age");
+
+  bool gender_found = false;
+  while (!gender_found) {
+    char choice;
+    printf("Input patient gender, (M)ale or (F)emale\n");
+    choice = getchar();
+    clearInputBuffer();
+
+    switch (choice) {
+    case 'M':
+    case 'm':
+      patient.gender = Male;
+      gender_found = true;
+      break;
+    case 'F':
+    case 'f':
+      patient.gender = Female;
+      gender_found = true;
+      break;
+    default:
+      printf("We only recognize two genders ;). Try again sissy!\n");
+    }
+  }
+
+  input_vitals(&patient);
+}
+
+void input_vitals(struct Patient *patient) {
+  bool airway_found = false;
+  while (!airway_found) {
+    char choice;
+    printf("Is the airway: (B)locked (F)ree or (I)nspiratory Stridor?\n");
+    choice = getchar();
+    clearInputBuffer();
+
+    switch (choice) {
+    case 'B':
+    case 'b':
+      patient->vitals->airway = Blocked;
+      airway_found = true;
+      break;
+    case 'F':
+    case 'f':
+      patient->vitals->airway = Free;
+      airway_found = true;
+      break;
+    case 'I':
+    case 'i':
+      patient->vitals->airway = Inspiratory_stridor;
+      airway_found = true;
+      break;
+    default:
+      printf("Invalid input. Try again!");
+    }
+  }
+
+  bool oxygen_saturation_found = false;
+  while (!oxygen_saturation_found) {
+    printf("Input patient Oxygen Saturation\n");
+    int res = scanf("%d", &patient->vitals->oxygen_saturation);
+    clearInputBuffer();
+    if (res != 1) {
+      printf("Invalid input. Try again!\n");
+    } else {
+      oxygen_saturation_found = true;
+    }
+  }
+
+  bool with_oxygen_found = false;
+  while (!with_oxygen_found) {
+    printf("Is the patient with oxygen? (Y)es  (N)o\n");
+    char choice;
+    choice = getchar();
+    clearInputBuffer();
+    switch (choice) {
+    case 'Y':
+    case 'y':
+      patient->vitals->with_oxygen = true;
+      with_oxygen_found = true;
+      break;
+    case 'N':
+    case 'n':
+      patient->vitals->with_oxygen = false;
+      with_oxygen_found = true;
+      break;
+    default:
+      printf("Invalid input. Try again!");
+    }
+  }
+
+  bool oxygen_saturation_found = false;
+  while (!oxygen_saturation_found) {
+    printf("Input patient Oxygen Saturation\n");
+    int res = scanf("%d", &patient->vitals->oxygen_saturation);
+    clearInputBuffer();
+    if (res != 1) {
+      printf("Invalid input. Try again!\n");
+    } else {
+      oxygen_saturation_found = true;
+    }
+  }
+}
+
+void clearInputBuffer() {
+  int c;
+  while ((c = getchar()) != '\n' && c != EOF) {
+  }
 }

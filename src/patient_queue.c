f9e3f94b94b4e1a3af43deee617aeb916623d3f5
Mads Ludvig Timm Fagerlund <mads@fagerlund.io>
2023-12-07 14:48:00 +0100

    Remove prints, add patient remove

--- src/patient_queue.c
+++ src/patient_queue.c
@@ -1,5 +1,6 @@
 #include "patient_queue.h"
 #include "triage_level.h"
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -10,12 +11,47 @@ struct patient_node *insert_tail(struct patient_node *cl, void *el) {
   new_node->next = NULL;
 
   if (cl == NULL) {
-    printf("list empty\n");
     return new_node;
   }
-  printf("list populated\n");
   cl->next = new_node;
   return cl;
+}
+
+void remove_patient_from_queue(struct patient_queue *pq, int p_id) {
+  pq->red_head = remove_patient_from_list(pq->red_head, p_id);
+  pq->orange_head = remove_patient_from_list(pq->orange_head, p_id);
+  pq->yellow_head = remove_patient_from_list(pq->yellow_head, p_id);
+  pq->green_head = remove_patient_from_list(pq->green_head, p_id);
+  pq->blue_head = remove_patient_from_list(pq->blue_head, p_id);
+}
+
+struct patient_node *remove_patient_from_list(struct patient_node *sl,
+                                              int patient_id) {
+  if (sl == NULL) {
+    return sl;
+  }
+
+  struct patient_node *cur, *prev;
+  cur = sl;
+  prev = NULL;
+
+  do {
+    struct Patient *patient = (struct Patient *)cur->data;
+    if (patient->id == patient_id) {
+
+      if (prev == NULL) {
+        struct patient_node *new_head = cur->next;
+        free(cur);
+        return new_head;
+      } else {
+        prev->next = cur->next;
+        free(cur);
+      }
+    }
+    prev = cur;
+    cur = cur->next;
+  } while (cur != NULL);
+  return sl;
 }
 
 void add_patient_to_queue(struct patient_queue *patient_queue,
@@ -26,12 +62,15 @@ void add_patient_to_queue(struct patient_queue *patient_queue,
 
   // Check if memory allocation is successful
   if (new_patient == NULL) {
-    fprintf(stderr, "Error: Unable to allocate memory for patient\n");
+    printf("Error: Unable to allocate memory for patient\n");
     exit(EXIT_FAILURE);
   }
 
   // Copy patient data
   *new_patient = patient;
+
+  new_patient->id = patient_queue->current_id;
+  patient_queue->current_id++;
 
   enum Triage_Level patient_triage_level = get_triage(*new_patient);
 
@@ -59,7 +98,7 @@ void add_patient_to_queue(struct patient_queue *patient_queue,
 }
 
 void print_patient(struct Patient *patient) {
-  printf("%s %d\n", patient->name, patient->age);
+  printf("name:%s age:%d id:%d\n", patient->name, patient->age, patient->id);
 }
 
 void print_circular_patient_list(struct patient_node *cl) {

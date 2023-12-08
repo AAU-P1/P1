d65bff7daba1ed0478881fd53a9810cb2228053b
Mads Ludvig Timm Fagerlund <mads@fagerlund.io>
2023-12-08 10:38:25 +0100

    Edit contaminant symptom

--- src/symptoms/18_contaminant.c
+++ src/symptoms/18_contaminant.c
@@ -1,33 +1,34 @@
 //
 // Created by Mathias Birkholm on 07/12/2023.
 //
-#include "18_contaminant.h.h"
+#include "18_contaminant.h"
+#include "stdbool.h"
 
-enum Triage_Level get_symptoms_contaminant_triage(struct contaminant contaminant) {
-    enum Triage_Level triage_level = Green;
+enum Triage_Level
+get_symptoms_contaminant_triage(struct Contaminant contaminant) {
+  enum Triage_Level triage_level = Green;
 
-    bool red = contaminant.sus_airway_block == stridor
+  bool red = contaminant.sus_airway_block == CSAB_Stridor;
 
-    bool orange = contaminant.if_consumet == battery ||
-    contaminant.sus_airway_block == affected_general_condition ||
-    contaminant.sus_stuck_object == general_condition;
+  bool orange = contaminant.if_consumed == CIC_Battery ||
+                contaminant.sus_airway_block == affected_general_condition ||
+                contaminant.sus_stuck_object == general_condition;
 
-    bool yellow = contaminant.if_consumet == pointy_or_sharp ||
-            contaminant.sus_airway_block == not_affected;
+  bool yellow = contaminant.if_consumed == pointy_or_sharp ||
+                contaminant.sus_airway_block == not_affected;
 
-    bool green = contaminant.if_consumet == rounded_objects ||
-            contaminant.sus_stuck_object == not_affected;
+  bool green = contaminant.if_consumed == rounded_objects ||
+               contaminant.sus_stuck_object == not_affected;
 
+  if (red) {
+    triage_level = Red;
+  } else if (orange) {
+    triage_level = Orange;
+  } else if (yellow) {
+    triage_level = Yellow;
+  } else if (green) {
+    triage_level = Green;
+  }
 
-    if (red) {
-        triage_level = Red;
-    } else if (orange) {
-        triage_level = Orange;
-    } else if (yellow) {
-        triage_level = Yellow;
-    } else if (green) {
-        triage_level = Green;
-    }
-
-    return triage_level;
+  return triage_level;
 }
--- src/symptoms/18_contaminant.h
+++ src/symptoms/18_contaminant.h
@@ -3,17 +3,31 @@
 #include "../triage_level.h"
 #include <stdbool.h>
 
-enum Contaminant_if_consumet { CIC_Battery, CIC_Sharp_and_pointy, CIC_Rounded_object, CIC_none };
-enum Contaminant_sus_struck_object { CSSO_General_condition, CSSO_Not_affected, CSSO_none };
-enum Contaminant_sus_airway_blockage {CSAB_Stridor, CSAB_General_condtion, CSAB_Not_affected, CSAB_none};
-
-struct Contaminant {
-    enum Contaminant_if_consumet if_consumet;
-    enum Contaminant_sus_stuck_object sus_stuck_object;
-    enum Contaminant_sus_airway_blockage sus_airway_block;
+enum Contaminant_if_consumed {
+  CIC_Battery,
+  CIC_Sharp_and_pointy,
+  CIC_Rounded_object,
+  CIC_none
+};
+enum Contaminant_sus_stuck_object {
+  CSSO_General_condition,
+  CSSO_Not_affected,
+  CSSO_none
+};
+enum Contaminant_sus_airway_blockage {
+  CSAB_Stridor,
+  CSAB_General_condtion,
+  CSAB_Not_affected,
+  CSAB_none
 };
 
-enum Triage_Level get_symptoms_contaminant_triage(struct Contaminant contaminant);
+struct Contaminant {
+  enum Contaminant_if_consumed if_consumed;
+  enum Contaminant_sus_stuck_object sus_stuck_object;
+  enum Contaminant_sus_airway_blockage sus_airway_block;
+};
+
+enum Triage_Level
+get_symptoms_contaminant_triage(struct Contaminant contaminant);
 
 #endif
-

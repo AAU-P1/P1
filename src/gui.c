4e6f001646913595fdad09fbfc2e9f904ba4dbc6
Mads Ludvig Timm Fagerlund <mads@fagerlund.io>
2023-12-08 12:22:56 +0100

    Move functions from main into gui library

---
+++ src/gui.c
@@ -0,0 +1,77 @@
+#include "gui.h"
+#include <stdbool.h>
+#include <stdio.h>
+void input_int(char *message, int *dest) {
+  while (true) {
+    printf("%s\n", message);
+    int res = scanf("%d", dest);
+    clearInputBuffer();
+    if (res != 1) {
+      printf("Invalid input. Try again!\n");
+    } else {
+      return;
+    }
+  }
+}
+
+void input_int_with_range(char *message, int *dest, int valid_low,
+                          int valid_high) {
+  while (true) {
+    printf("%s\n", message);
+    int res = scanf("%d", dest);
+    clearInputBuffer();
+    if (res != 1 || *dest > valid_high || *dest < valid_low) {
+      printf("Invalid input. Try again!\n");
+    } else {
+      return;
+    }
+  }
+}
+
+void input_double(char *message, double *dest) {
+  while (true) {
+    printf("%s\n", message);
+    int res = scanf("%lf", dest);
+    clearInputBuffer();
+    if (res != 1) {
+      printf("Invalid input. Try again!\n");
+    } else {
+      return;
+    }
+  }
+}
+
+void input_char(char *choice, char *message, char *valid_chars) {
+  while (true) {
+    printf("%s\n", message);
+    char c = getchar();
+    clearInputBuffer();
+    int i = 0;
+    while (valid_chars[i] != '\0') {
+      if (c == valid_chars[i]) {
+        *choice = c;
+        return;
+      }
+      ++i;
+    }
+    printf("Invalid input. Try again!\n");
+  }
+}
+
+void input_string(char *message, char *dest) {
+  while (true) {
+    printf("%s\n", message);
+    int res = scanf("%s", dest);
+    if (res != 1) {
+      printf("Invalid input. Try again!\n");
+    } else {
+      return;
+    }
+  }
+}
+
+void clearInputBuffer() {
+  int c;
+  while ((c = getchar()) != '\n' && c != EOF) {
+  }
+}
---
+++ src/gui.h
@@ -0,0 +1,12 @@
+void input_int(char *message, int *dest);
+
+void input_int_with_range(char *message, int *dest, int valid_low,
+                          int valid_high);
+
+void input_double(char *message, double *dest);
+
+void input_char(char *choice, char *message, char *valid_chars);
+
+void input_string(char *message, char *dest);
+
+void clearInputBuffer();

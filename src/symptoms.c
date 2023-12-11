8cb7218b4ae78d54080b78a782736c2109f2f63e
Anton <amve23@student.aau.dk>
2023-12-11 11:03:32 +0100

    symptoms

--- src/symptoms.c
+++ src/symptoms.c
@@ -58,6 +58,7 @@ void input_symptoms(struct symptom_node *sn) {
   clear_screen();
   input_int_with_range("Please choose one of the following:\n(1) or ...",
                        &choice, 1, 1);
+
   switch (choice) {
   case 1:
     sn->symptom_id = choice;

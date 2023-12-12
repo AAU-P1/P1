7acc00198e3a668a92ab8ad4612f4d7ea93fc149
Mads Ludvig Timm Fagerlund <mads@fagerlund.io>
2023-12-12 15:12:06 +0100

    Add auto save and load of file

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include "controller.h"
+#include "file.h"
 #include "patient.h"
 #include "symptoms.h"
 #include "symptoms/001_abstinence.h"
@@ -11,31 +12,40 @@ int main(void) {
 
   struct patient_queue patient_queue = {NULL, NULL, NULL, NULL, NULL, 1};
 
+  load_patient_queue_from_file(&patient_queue);
+
   char choice;
   clear_screen();
+  print_queue(&patient_queue);
   while (true) {
-    printf("(T)riage new patient      (I)nput patient from file       (R)emove "
-           "Patient         (D)isplay Queue        (Q)uit\n");
+    printf("(T)riage new patient | (R)emove Patient | (D)isplay Queue "
+           "| Display (P)OV Queue | (Q)uit\n");
     choice = getchar();
-    clearInputBuffer();
+    clear_input_buffer();
 
     switch (choice) {
     case 'T':
-    case 't':
-      add_patient_to_queue(&patient_queue, input_patient());
+    case 't': {
+      struct Patient *patient = input_patient();
+      get_triage(patient);
+      add_patient_to_queue(&patient_queue, patient, patient_queue.current_id);
+      patient_queue.current_id++;
+      save_patient_queue_to_file(&patient_queue);
       print_queue(&patient_queue);
       break;
-    case 'I':
-    case 'i':
-      break;
+    }
     case 'R':
     case 'r':
       remove_patient(&patient_queue);
-      print_queue(&patient_queue);
+      save_patient_queue_to_file(&patient_queue);
       break;
     case 'D':
     case 'd':
       print_queue(&patient_queue);
+      break;
+    case 'P':
+    case 'p':
+      print_patient_pov(&patient_queue);
       break;
     case 'Q':
     case 'q':

cd782e9ea93742f3a73bd2ed1392c1db98380646
Mads Ludvig Timm Fagerlund <mads@fagerlund.io>
2023-12-13 21:22:17 +0100

    Move menu functionality into menu library

---
+++ src/menu.c
@@ -0,0 +1,55 @@
+#include "controller.h"
+#include "file.h"
+#include "patient.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int start_menu(void) {
+
+  struct patient_queue patient_queue = {NULL, NULL, NULL, NULL, NULL, 1};
+
+  load_patient_queue_from_file(&patient_queue);
+
+  char choice;
+  clear_screen();
+  print_queue(&patient_queue);
+  while (true) {
+    printf("(T)riage new patient | (R)emove Patient | (D)isplay Queue "
+           "| Display (P)OV Queue | (Q)uit\n");
+    choice = getchar();
+    clear_input_buffer();
+
+    switch (choice) {
+    case 'T':
+    case 't': {
+      struct Patient *patient = input_patient();
+      get_triage(patient);
+      add_patient_to_queue(&patient_queue, patient, patient_queue.current_id);
+      patient_queue.current_id++;
+      save_patient_queue_to_file(&patient_queue);
+      print_queue(&patient_queue);
+      break;
+    }
+    case 'R':
+    case 'r':
+      remove_patient(&patient_queue);
+      save_patient_queue_to_file(&patient_queue);
+      break;
+    case 'D':
+    case 'd':
+      print_queue(&patient_queue);
+      break;
+    case 'P':
+    case 'p':
+      print_patient_pov(&patient_queue);
+      break;
+    case 'Q':
+    case 'q':
+      printf("Bye!\n");
+      exit(1);
+      break;
+    default:
+      printf("Invalid input try again\n");
+    }
+  }
+}
